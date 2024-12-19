88a29bd7c51ec75706cfa26060eba1823627e780
oliwiawaszczuk <oliwiawaszczuk12321@gmail.com>
2024-12-19 19:50:28 +0100

    enable to zooming image in ImageLook.tsx

--- frontend/screens/ImageLook.tsx
+++ frontend/screens/ImageLook.tsx
@@ -3,25 +3,24 @@ import {useRoute} from "@react-navigation/native"
 import React from "react"
 import {Image, View, StyleSheet, Animated} from "react-native"
 import {Text} from "../components/Text/Default"
-import {PinchGestureHandler} from "react-native-gesture-handler";
+import { ImageZoom } from '@likashefqet/react-native-image-zoom'
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 export default function ImageLook() {
     const route = useRoute()
     const {pathToImage} = route.params as { pathToImage: string }
 
     return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.imageContainer}>
-                <Image
-                    source={{uri: pathToImage}}
-                    style={ styles.image }
-                    resizeMode="contain"
-                />
-            </View>
-            <View style={styles.footer}>
-                <Text>Settings</Text>
-            </View>
-        </SafeAreaView>
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <ImageZoom uri={pathToImage} style={styles.image}/>
+                </View>
+                <View style={styles.footer}>
+                    <Text>Settings</Text>
+                </View>
+            </SafeAreaView>
+        </GestureHandlerRootView>
     )
 }
 
